c4aae2015964c1d91b00f2396e5cca64be2102e0
Kryštof Korb <krystof@korb.cz>
2023-02-09 19:19:08 +0100

    Add setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p anthony
+cd anthony
+
+curl -fsSL https://raw.githubusercontent.com/krystof-k/anthony/main/scripts/all.sh -O
+curl -fsSL https://raw.githubusercontent.com/krystof-k/anthony/main/scripts/check.sh -O
+curl -fsSL https://raw.githubusercontent.com/krystof-k/anthony/main/scripts/performance.ini -O
+curl -fsSL https://raw.githubusercontent.com/krystof-k/anthony/main/scripts/performance.sh -O
+curl -fsSL https://raw.githubusercontent.com/krystof-k/anthony/main/scripts/smart.sh -O
+curl -fsSL https://raw.githubusercontent.com/krystof-k/anthony/main/scripts/utilities.sh -O
+curl -fsSL https://raw.githubusercontent.com/krystof-k/anthony/main/scripts/variables.sh -O
+curl -fsSL https://raw.githubusercontent.com/krystof-k/anthony/main/scripts/wipe.sh -O
